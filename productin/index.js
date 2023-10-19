20334602e658477e5758e1590a5a6af184078252
Tomasz Abratowski <tomasz.abratowski@wpengine.com>
2023-10-19 14:14:57 +0200

    testpr

---
+++ productin/index.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 3300;
+
+server.use(express.static('public'));
+
+server.get('/', (_req, res) => {
+  res.send('Hello Express ver 2.0! kalafior!');
+});
+
+server.get('/fetch-wordpress-graphql', async (_req, res) => {
+  const query = `
+    query QueryPosts {
+      posts {
+        nodes {
+          id
+          content
+          title
+          slug
+          featuredImage {
+            node {
+              mediaDetails {
+                sizes {
+                  sourceUrl
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const response = await axios.post(process.env.GRAPHQL_API_URL, { query });
+  res.send(response.data.data.posts.nodes);
+});
+
+server.get('/envs', (req, res) => {
+  console.log('hello!! rebuild');
+  console.log(process.env);
+  res.send('Envs displayed in logs!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Application is listening at port ${PORT}`);
+});
